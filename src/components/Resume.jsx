0fff393a575f21a0cb6508c90a5b3835ad576e14
Arael <araeltiejerina@gmial.com>
2023-08-23 12:12:33 -0500

    made website mobile-friendly

--- src/components/Resume.jsx
+++ src/components/Resume.jsx
@@ -1,8 +1,6 @@
-// import statements for all the icons used on the resume page of the website 
 import React from 'react';
-import Doc from "../assets/docs/resume.pdf"
+import Doc from '../assets/docs/resume.pdf';
 import {
-  FaGitAlt,
   FaJs,
   FaGithub,
   FaHtml5,
@@ -12,6 +10,7 @@ import {
   FaReact,
   FaNpm,
   FaNetworkWired,
+  FaDownload,
 } from 'react-icons/fa';
 import { DiJqueryLogo, DiMongodb } from 'react-icons/di';
 import {
@@ -19,18 +18,18 @@ import {
   SiHeroku,
   SiWebpack,
   SiGraphql,
+  SiTailwindcss,
 } from 'react-icons/si';
-// component that will be exported to App.js and rendered on the resume page of the website
+
 const Resume = () => {
   return (
-    <div name='Resume' className='w-full h-screen bg-gradient-to-b from-[#989FCE] to-[#D3F9B5] text-[#393D3F]'>
-      
-      <div className='max-w-[1000px] mx-auto p-8 flex flex-col justify-center w-full h-full'>
+    <div name='Resume' className='w-full h-auto md:h-screen bg-gradient-to-b from-[#989FCE] to-[#D3F9B5] text-[#393D3F]'>
+      <div className='max-w-[1000px] mx-auto p-6 md:p-8 flex flex-col justify-center w-full h-full'>
         <div>
-          <p className='text-5xl font-bold inline border-b-4 border-[#F2A65A] '>Resume</p>
-          <p className='py-6 text-xl'>Profficient with:</p>
+          <p className='text-4xl md:text-5xl font-bold inline border-b-4 border-[#F2A65A] '>Resume</p>
+          <p className='py-4 text-xl'>Proficient with:</p>
         </div>
-        <div className='w-full grid grid-cols-2 sm:grid-cols-3 gap-8 text-center'>
+        <div className='grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-4 md:gap-8 text-center'>
           <div className='shadow-md hover:scale-110 duration-300 rounded-lg p-6'>
             <FaHtml5 size={60} className='mx-auto mb-4' />
             <p className='text-lg'>HTML</p>
@@ -64,7 +63,7 @@ const Resume = () => {
             <p className='text-lg'>Npm</p>
           </div>
           <div className='shadow-md hover:scale-110 duration-300 rounded-lg p-6'>
-          <FaNetworkWired size={60} className='mx-auto mb-4' />
+            <FaNetworkWired size={60} className='mx-auto mb-4' />
             <p className='text-lg'>APIs</p>
           </div>
           <div className='shadow-md hover:scale-110 duration-300 rounded-lg p-6'>
@@ -91,16 +90,23 @@ const Resume = () => {
             <SiWebpack size={60} className='mx-auto mb-4' />
             <p className='text-lg'>Webpack</p>
           </div>
+          <div className='shadow-md hover:scale-110 duration-300 rounded-lg p-6'>
+            <SiTailwindcss size={60} className='mx-auto mb-4' />
+            <p className='text-lg'>TailWind Css</p>
+          </div>
+          {/* Add more skill blocks here */}
         </div>
       </div>
-      
-      <div className='text-center py-6'>
-      <a
+      <div className='text-center py-4 md:py-6'>
+        <a
           href={Doc}
-          className='inline-block px-6 py-3 bg-[#F2A65A] text-white rounded-lg shadow-md transition-colors duration-300 hover:bg-[#F58549]'
-          target="_blank"
-          rel="noopener noreferrer"
+          className='inline-flex items-center px-6 py-3 bg-[#F2A65A] text-white rounded-lg shadow-md transition-colors duration-300 hover:bg-[#F58549]'
+          target='_blank'
+          rel='noopener noreferrer'
         >
+          <span className='mr-2'>
+            <FaDownload />
+          </span>
           Download Resume
         </a>
       </div>
